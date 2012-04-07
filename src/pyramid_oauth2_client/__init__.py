a8cac6615c5948d86772a02b19998a4c8b6e61d3
Jason Kölker <jason@koelker.net>
2012-04-06 20:54:01 -0500

    Allow config.include to augment request objects

--- src/pyramid_oauth2_client/__init__.py
+++ src/pyramid_oauth2_client/__init__.py
@@ -1,3 +1,5 @@
+from pyramid import events
+
 from pyramid_oauth2_client.facebook import FacebookClient
 from pyramid_oauth2_client.github import GithubClient
 from pyramid_oauth2_client.yasso import YassoClient
@@ -9,8 +11,12 @@ clients = {"facebook": FacebookClient,
           }
 
 
-def oauth2_client(provider, request=None):
+def oauth2_client(request, provider):
     """Return an OAuth2 client for the provider
+
+    ``request``
+        A Pyramid request object.
+        Required.
 
     ``provider``
         The provider name to return the client for (str)
@@ -20,12 +26,21 @@ def oauth2_client(provider, request=None):
             facebook
             github
             yasso
-
-    ``request``
-        A Pyramid request object. If provided the provider client class
-        will be instantiated with the request.
     """
-    client = clients[provider]
-    if request:
-        client = client(request)
-    return client
+    return clients[provider](request)
+
+
+def includeme(config):
+    """Set up standard configurator registrations. Use via:
+
+    .. code-block:: python
+
+    config = Configurator()
+    config.include('pyramid_oauth2_client')
+    """
+
+    def new_request(event):
+        request = event.request
+        request.oauth2_client = lambda prov: oauth2_client(request, prov)
+
+    config.add_subscriber(new_request, events.NewRequest)
